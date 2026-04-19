c0098963472dca5d47bbd9a36993025097f591b8
Hafsat Mukhtar <hafsatmukhtargaya@gmail.com>
2026-04-19 22:06:45 +0100

    feat: add task input and dynamic task display functionality

--- script.js
+++ script.js
@@ -22,3 +22,52 @@ function liveClock() {
     document.querySelector(".dynamic-greeting").textContent = greetingString;
 }  
 setInterval(liveClock, 1000); 
+
+let totalSeconds = 1500;
+let timerInterval = null;
+function startTimer() {
+    if (timerInterval != null) {
+        return;
+    }
+    timerInterval = setInterval(function() {
+        totalSeconds = totalSeconds - 1;
+        let minutes = String(Math.floor(totalSeconds/60)).padStart(2,"0");
+        let seconds = String(totalSeconds % 60).padStart(2, "0");
+        let timerString = minutes + ":" + seconds;
+        document.querySelector(".timer").textContent = timerString
+    }, 1000)
+}
+
+function pauseTimer() {
+    clearInterval (timerInterval);
+    timerInterval = null;
+}
+
+function resetTimer () {
+    totalSeconds = 1500;
+    clearInterval (timerInterval)
+    timerInterval = null;
+    totalSeconds = 1500;
+    document.querySelector(".timer").textContent = "25:00"; 
+}
+
+let tasks = [];
+function addTasks() {
+    let userInput = document.querySelector(".input-task").value;
+    if (userInput === "") {
+        return;
+    }
+    tasks.push({text: userInput, done: false})
+    document.querySelector(".input-task").value = ""
+    displayTasks();
+}
+
+function displayTasks() {
+    document.querySelector("#task-list").innerHTML = "";
+    tasks.forEach(function(task) {
+        let taskDiv = document.createElement("div");
+        taskDiv.innerHTML = "<input type = 'checkbox'>" + "<span>" + task.text + "</span>" + "<button>🗑</button>" ;
+        taskDiv.className = "task-item"
+        document.querySelector("#task-list").appendChild(taskDiv)
+    });
+}
